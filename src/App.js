34fb372e8e7c22137b5704762940662d98ef9382
Andrew Ayman <maria.ayman1877@gmail.com>
2021-07-29 15:00:43 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      Person: {
+        fullName: "Maria Ayman",
+        bio: "Junior React Developer",
+        imgSrc: "/profile.png",
+        profession: "Engineer"
+      },
+      shows: false,
+      msg: "Show Profile",
+      //date: new Date()
+      sec: 0
+    };
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    // this.setState({
+    //   date: new Date()
+    // });
+    this.setState({
+      sec: this.state.sec + 1
+    });
+  }
+
+  showProfile = () => {
+    this.setState({
+      shows: !this.state.shows,
+      msg: ((this.state.shows === true) ? "Show Profile" : "Hide Profile")
+    });
+    console.log(this.state.shows);  
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <button onClick={this.showProfile} className="button">{this.state.msg}</button>
+        {this.state.shows && (
+          <div className="profileCont">
+            <img src={this.state.Person.imgSrc} alt="" className="profileImg"/>
+            <div>
+              <p><span className="span">Full Name:</span> {this.state.Person.fullName}</p>
+              <p><span className="span">Bio:</span> {this.state.Person.bio}</p>
+              <p><span className="span">Profession:</span> {this.state.Person.profession}</p>
+            </div>
+          </div>
+        )}
+        <h2 style={{color: "#fff"}}>It is {this.state.sec} secounds since mounting</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
